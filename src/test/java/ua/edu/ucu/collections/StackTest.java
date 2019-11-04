e1dcf8fca2e446baa16c39467f3703174238aa72
usach <usachevavika171@gmail.com>
2019-11-04 11:39:26 +0200

    Final commit

--- src/test/java/ua/edu/ucu/collections/StackTest.java
+++ src/test/java/ua/edu/ucu/collections/StackTest.java
@@ -4,9 +4,52 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StackTest {
-    
+
     @Test
-    public void testSomeMethod() {
+    public void testPeek() throws Exception {
+        Stack myStack = new Stack();
+        myStack.push(2);
+        myStack.push(3);
+
+        assertEquals(myStack.peek(), 3);
+
+        myStack.push(5);
+        myStack.push(7);
+
+        assertEquals(myStack.peek(), 7);
     }
-    
+
+    @Test
+    public void testEnqueue() throws Exception {
+        Stack myStack = new Stack();
+        myStack.push(2);
+        assertEquals(myStack.peek(), 2);
+
+        myStack.push(3);
+        myStack.pop();
+        assertEquals(myStack.peek(), 2);
+
+    }
+
+    @Test
+    public void testDequeue() throws Exception {
+        Stack myStack = new Stack();
+        myStack.push(2);
+        assertEquals(myStack.peek(), 2);
+        myStack.push(3);
+        assertEquals(myStack.peek(), 3);
+
+        myStack.push(5);
+        myStack.push(6);
+        myStack.pop();
+        myStack.push(3);
+
+        myStack.pop();
+        myStack.pop();
+        myStack.pop();
+        myStack.pop();
+        myStack.push(1);
+        assertEquals(myStack.peek(), 1);
+
+    }
 }
